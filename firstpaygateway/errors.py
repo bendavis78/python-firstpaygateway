df01884152de5cf6290e6629f72034ef86f07ceb
Ben Davis <bendavis78@gmail.com>
2017-01-06 17:05:39 -0600

    fixes error when validation exception is thrown

--- firstpaygateway/errors.py
+++ firstpaygateway/errors.py
@@ -3,7 +3,7 @@ class GatewayError(Exception):
         self.result = result
         if result.validation_has_failed:
             self.error_messages = [
-                e.message for e in self.validation_failures
+                e.message for e in result.validation_failures
             ]
         else:
             self.error_messages = result.error_messages
@@ -11,6 +11,4 @@ class GatewayError(Exception):
 
 
 class GatewayValidationError(GatewayError):
-    def __init__(self, result):
-        self.validation_failures = result.validation_failures
-        super(Exception, self).__init__(self.error_messages[0])
+    pass
